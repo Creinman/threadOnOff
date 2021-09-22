a06ce6fb8a394f36d526e581a9e42bac47043fb3
Oleg <sosolala404@gmail.com>
2021-09-22 17:09:43 +0300

    use AtomicBoolean

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,9 @@ class Main {
     public static void main(String[] args) {
         Toy onoff = new Toy();
         Thread threadUser = new Thread(null, onoff::user, "Юзер");
-        Thread threadUser2 = new Thread(null, onoff::user, "Юзер");
         Thread threadToy = new Thread(null, onoff::toy, "Игрушка");
-        Thread threadToy2 = new Thread(null, onoff::toy, "Игрушка");
         threadUser.start();
         threadToy.start();
-        threadUser2.start();
-        threadToy2.start();
-        //new User().start();
-        //new Toy().start();
     }
 }
 
--- src/Toy.java
+++ src/Toy.java
@@ -1,81 +1,25 @@
+import java.util.concurrent.atomic.AtomicBoolean;
 
-
-/*public class Toy extends Thread {
-    public static boolean tumbler;
-    int severalTimes = 5;
+class Toy {
+    public int count = 5;
     int i = 0;
+    public static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
     public void toy() {
-        synchronized (user) {
-            while (i < severalTimes) {
-                i = i + 1;
-                // if (!tumbler) {
-                System.out.println("Игрушка: тумблер OFF");
-                tumbler = false;
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        while (i < count) {
+            i++; //atomicBoolean.get());
+            if (atomicBoolean.compareAndSet(true, false)) {
+                System.out.println(Thread.currentThread().getName() + ": А я Выключил!");
             }
         }
     }
 
-    public synchronized void user() {
-
-        while (i < severalTimes) {
-            i = i + 1;
-            System.out.println("Пользователь: тумблер ON");
-            tumbler = true;
-            try {
-                notify();
-                //Thread.sleep(3000);
-                wait(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
-//}
-*/
-
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.*;
-import java.util.concurrent.locks.ReentrantLock;
-
-import static java.util.concurrent.locks.Condition.*;
-
-class Toy extends ReentrantLock {
-    Lock lock = new ReentrantLock();
-    Condition condition = newCondition();
     public void user() {
-        try {
-            lock.lock();
-            Thread.sleep(3000);
-            System.out.println("Пользователь включил");
-            condition.signal(); // Вместо notify();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
-    }
-    public void toy() {
-        int i = 0;
-        try {
-            lock.lock();
-        //    while (i<5) {
-                i = i + 1;
-                System.out.println("Игрушка выключила");
-                condition.await(); // Вместо wait();
-           // }
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
+        while (i < count) {
+            i++; //atomicBoolean.get());
+            if (atomicBoolean.compareAndSet(false, true)) {
+                System.out.println(Thread.currentThread().getName() + ": я Включила");
+            }
         }
     }
 }
